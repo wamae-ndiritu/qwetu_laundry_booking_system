6eeb09eacd84eee60f242393c4486b6e3708bb39
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-04-08 08:45:11 +0300

    Design navbar section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,29 +1,27 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 const Navbar = () => {
   return (
-    <div className="bg-violet-500 py-4m-1  flex justify-between items-center ">
-      <h1 className="text-white text-3xl font-bold m-3">Qwetu</h1>
-      <ul className="flex m-3 ">
-        <li>
-          <button className="p-1 m-1  text-white font-semibold ">
-            Services
-          </button>
-        </li>
-        <li>
-          <NavLink to="/login">
-            <button className="p-1 m-1 rounded text-violet-600 bg-white ">
-              Sign In
-            </button>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/register">
-            <button className="p-1 m-1  text-white rounded bg-black">
-              Sign Up
-            </button>
-          </NavLink>
-        </li>
-      </ul>
+    <div className='bg-slate-100  px-4 py-4 flex justify-between items-center'>
+      <h4 className='text-xl font-semibold'>Students Residence</h4>
+      <div className='flex items-center gap-3'>
+        <span className='bg-slate-50 w-8 h-8 rounded-full flex justify-center items-center text-gray-600 relative'>
+          <NotificationsIcon style={{ fontSize: "18px" }} />
+          <span className='bg-red-500 w-4 h-4 rounded-full text-white text-sm flex justify-center items-center badge'>
+            0
+          </span>
+        </span>
+        <span className='text-sm text-gray-600 text-right'>
+          <h6 className='uppercase'>Wamae Ndiritu</h6>
+        </span>
+        <Link to='/profile'>
+          <img
+            src='/profile.jpeg'
+            alt='profile'
+            className='h-10 w-10 rounded-full object-cover'
+          />
+        </Link>
+      </div>
     </div>
   );
 };
