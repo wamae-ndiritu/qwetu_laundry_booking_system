26a75f9ca3c86dceda684a93bd082bc31f6d3e81
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-05-06 10:26:17 +0300

    Configure Django-Cors-Headers

--- backend/qwetu/settings.py
+++ backend/qwetu/settings.py
@@ -28,12 +28,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
     'app'
 ]
 
 AUTH_USER_MODEL = 'app.CustomUser'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -42,6 +44,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'qwetu.urls'
 
